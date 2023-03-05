88fe9f114e45ab5b869c27d1d916bdb91a52a3db
alawee20oct <alawee147@hotmail.co.th>
2023-03-05 18:38:22 +0700

    ignore file

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import './App.css';
+
+import naruto from "./assets/images/naruto-shippuden.jpg";
+import reborn from "./assets/images/katekyo-hitman-reborn.jpg";
+import dragonball from "./assets/images/dragonball-z.jpg";
+import bleach from "./assets/images/bleach.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="mt-5 text-center">
+        <h2 className="text-dark">My Anime List</h2>
+      </div>
+
+      <div className="text-center mt-5 d-flex justify-content-center">
+        <div className="card d-block justify-content-center align-items-center mx-4" style={{width: "200px", height: "320px"}}>
+          <img src={naruto} className="card-img" alt='' style={{width: "100%", height: "100%", objectFit: "cover"}}/>
+          <div className="card-body">
+            <h5 className="card-title">Naruto Shippuden</h5>
+          </div>
+        </div>
+        <div className="card d-block justify-content-center align-items-center mx-4" style={{width: "200px", height: "320px"}}>
+          <img src={dragonball} className="card-img" alt='' style={{width: "100%", height: "100%", objectFit: "cover"}}/>
+          <div className="card-body">
+            <h5 className="card-title">Dragonball Z</h5>
+          </div>
+        </div>
+        <div className="card d-block justify-content-center align-items-center mx-4" style={{width: "200px", height: "320px"}}>
+          <img src={bleach} className="card-img" alt='' style={{width: "100%", height: "100%", objectFit: "cover"}}/>
+          <div className="card-body">
+            <h5 className="card-title">Bleach</h5>
+          </div>
+        </div>
+        <div className="card d-block justify-content-center align-items-center mx-4" style={{width: "200px", height: "320px"}}>
+          <img src={reborn} className="card-img" alt='' style={{width: "100%", height: "100%", objectFit: "cover"}}/>
+          <div className="card-body">
+            <h5 className="card-title">Katekyo Hitman Reborn!</h5>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
