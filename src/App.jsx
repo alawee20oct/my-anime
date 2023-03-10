5c69c4e666cac97e5348703bacb5f63de98040a5
alawee20oct <alawee147@hotmail.co.th>
2023-03-10 14:29:35 +0700

    set layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import naruto from "./assets/images/naruto-shippuden.jpg";
 import reborn from "./assets/images/katekyo-hitman-reborn.jpg";
 import dragonball from "./assets/images/dragonball-z.jpg";
 import bleach from "./assets/images/bleach.jpg";
+import titan from "./assets/images/attack-on-titan.jpg";
+import demonslayer from "./assets/images/kimetsu-no-yaiba.jpg";
 
 function App() {
   return (
@@ -13,32 +15,87 @@ function App() {
         <h2 className="text-dark">My Anime List</h2>
       </div>
 
-      <div className="text-center mt-5 d-flex justify-content-center">
-        <div className="card d-block justify-content-center align-items-center mx-4" style={{width: "200px", height: "320px"}}>
-          <img src={naruto} className="card-img" alt='' style={{width: "100%", height: "100%", objectFit: "cover"}}/>
-          <div className="card-body">
-            <h5 className="card-title">Naruto Shippuden</h5>
+      <section>
+        <div className="container mt-5">
+
+          <div className="row row-cols-xl-6 row-cols-lg-4 row-cols-md-2">
+
+            <div className="col">
+              <div className="card mx-auto h-100 border-0 text-center" style={{width: "160px", height: "240px"}}>
+                <img src={naruto} className="card-img" alt='' style={{width: "100%", height: "100%", objectFit: "cover"}}/>
+                <div className="card-body">
+                  <h6 className="card-title" style={{marginLeft: "-100%", marginRight: "-100%"}}>Naruto Shippuden</h6>
+                  <div class="progress" style={{height: "12px"}}>
+                    <div class="progress-bar bg-primary progress-bar-striped progress-bar-animated" role="progressbar" style={{width: "100%"}}></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col">
+              <div className="card mx-auto h-100 border-0 text-center" style={{width: "160px", height: "240px"}}>
+                <img src={dragonball} className="card-img" alt='' style={{width: "100%", height: "100%", objectFit: "cover"}}/>
+                <div className="card-body">
+                  <h6 className="card-title" style={{marginLeft: "-100%", marginRight: "-100%"}}>Dragonball Z</h6>
+                  <div class="progress" style={{height: "12px"}}>
+                    <div class="progress-bar bg-primary progress-bar-striped progress-bar-animated" role="progressbar" style={{width: "100%"}}></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col">
+              <div className="card mx-auto h-100 border-0 text-center" style={{width: "160px", height: "240px"}}>
+                <img src={bleach} className="card-img" alt='' style={{width: "100%", height: "100%", objectFit: "cover"}}/>
+                <div className="card-body">
+                  <h6 className="card-title" style={{marginLeft: "-100%", marginRight: "-100%"}}>Bleach</h6>
+                  <div class="progress" style={{height: "12px"}}>
+                    <div class="progress-bar bg-warning progress-bar-striped progress-bar-animated" role="progressbar" style={{width: "100%"}}></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col text-center">
+              <div className="card mx-auto h-100 border-0 text-center" style={{width: "160px", height: "240px"}}>
+                <img src={reborn} className="card-img" alt='' style={{width: "100%", height: "100%", objectFit: "cover"}}/>
+                <div className="card-body">
+                  <h6 className="card-title" style={{marginLeft: "-100%", marginRight: "-100%"}}>Katekyo Hitman Reborn!</h6>
+                  <div class="progress" style={{height: "12px"}}>
+                    <div class="progress-bar bg-primary progress-bar-striped progress-bar-animated" role="progressbar" style={{width: "100%"}}></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col text-center">
+              <div className="card mx-auto h-100 border-0 text-center" style={{width: "160px", height: "240px"}}>
+                <img src={titan} className="card-img" alt='' style={{width: "100%", height: "100%", objectFit: "cover"}}/>
+                <div className="card-body">
+                  <h6 className="card-title" style={{marginLeft: "-100%", marginRight: "-100%"}}>Attack on Titan</h6>
+                  <div class="progress" style={{height: "12px"}}>
+                    <div class="progress-bar bg-danger progress-bar-striped progress-bar-animated" role="progressbar" style={{width: "100%"}}></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col text-center">
+              <div className="card mx-auto h-100 border-0 text-center" style={{width: "160px", height: "240px"}}>
+                <img src={demonslayer} className="card-img" alt='' style={{width: "100%", height: "100%", objectFit: "cover"}}/>
+                <div className="card-body">
+                  <h6 className="card-title" style={{marginLeft: "-100%", marginRight: "-100%"}}>Demon Slayer</h6>
+                  <div class="progress" style={{height: "12px"}}>
+                    <div class="progress-bar bg-danger progress-bar-striped progress-bar-animated" role="progressbar" style={{width: "100%"}}></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
           </div>
+
         </div>
-        <div className="card d-block justify-content-center align-items-center mx-4" style={{width: "200px", height: "320px"}}>
-          <img src={dragonball} className="card-img" alt='' style={{width: "100%", height: "100%", objectFit: "cover"}}/>
-          <div className="card-body">
-            <h5 className="card-title">Dragonball Z</h5>
-          </div>
-        </div>
-        <div className="card d-block justify-content-center align-items-center mx-4" style={{width: "200px", height: "320px"}}>
-          <img src={bleach} className="card-img" alt='' style={{width: "100%", height: "100%", objectFit: "cover"}}/>
-          <div className="card-body">
-            <h5 className="card-title">Bleach</h5>
-          </div>
-        </div>
-        <div className="card d-block justify-content-center align-items-center mx-4" style={{width: "200px", height: "320px"}}>
-          <img src={reborn} className="card-img" alt='' style={{width: "100%", height: "100%", objectFit: "cover"}}/>
-          <div className="card-body">
-            <h5 className="card-title">Katekyo Hitman Reborn!</h5>
-          </div>
-        </div>
-      </div>
+      </section>
     </>
   );
 }
