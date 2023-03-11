fa3c8243fdbfd24deefab4bac647567c7e7e254d
alawee20oct <alawee147@hotmail.co.th>
2023-03-12 00:38:52 +0700

    add icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,45 +1,68 @@
 import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
-import { Container, Navbar, Nav } from 'react-bootstrap';
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 function Header() {
-  return (
-    <>
-        <section>
-            <Navbar collapseOnSelect expand="md" bg="light" variant="light">
-                <Container>
-                    <Navbar.Collapse className="justify-content-between">
-                        <Nav>
-                            <Nav.Item>
-                                <Nav.Link href="#">Home</Nav.Link>
-                            </Nav.Item>
-                        </Nav>
-                        <Nav>
-                            <Nav.Item>
-                                <Nav.Link href="#">Watching</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link href="#">Will Watch</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link href="#">Watched</Nav.Link>
-                            </Nav.Item>
-                        </Nav>
-                        <Nav>
-                            <Nav.Item>
-                                <Nav.Link href="#">Setting</Nav.Link>
-                            </Nav.Item>
-                        </Nav>
-                    </Navbar.Collapse>
+    return (
+        <>
+            <Navbar expand="md" bg="light" variant="light" fixed="top">
+                <Container fluid>
+                    <Navbar.Toggle className="" aria-controls="header-nav"/>
+                    <Navbar.Offcanvas id="header-nav" aria-labelledby="title-nav" placement="end">
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id="title-nav">MENU</Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body className="justify-content-between">
+                            <Nav>
+                                <Nav.Item>
+                                    <Nav.Link href="#" className="d-flex align-items-center">
+                                        <i className="bi bi-house-door-fill text-dark fs-4 me-2"></i>
+                                        <Navbar.Text className="d-md-none">Home</Navbar.Text>
+                                    </Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                            <hr className="d-md-none"/>
+                            <Nav>
+                                <Nav.Item>
+                                    <Nav.Link href="#" className="d-flex align-items-center">
+                                        <i className="bi bi-play-btn-fill text-danger fs-4 me-2"></i>
+                                        <Navbar.Text className="d-md-none">Now Watching</Navbar.Text>
+                                    </Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link href="#" className="d-flex align-items-center">
+                                        <i className="bi bi-pin-angle-fill text-primary fs-4 me-2"></i>
+                                        <Navbar.Text className="d-md-none">Will Watch</Navbar.Text>
+                                    </Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link href="#" className="d-flex align-items-center">
+                                        <i className="bi bi-check-circle-fill text-success fs-4 me-2"></i>
+                                        <Navbar.Text className="d-md-none">Already Watched</Navbar.Text>
+                                    </Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                            <hr className="d-md-none"/>
+                            <Nav>
+                                <Nav.Item >
+                                    <Nav.Link href="#" className="d-flex align-items-center">
+                                        <i className="bi bi-gear-fill text-dark fs-4 me-2"></i>
+                                        <Navbar.Text className="d-md-none">Setting</Navbar.Text>
+                                    </Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Offcanvas.Body>
+                    </Navbar.Offcanvas>
                 </Container>
-                    <Navbar.Toggle className="me-3" aria-controls="responsive-navbar-nav" />
             </Navbar>
-        </section>
-    </>
-  )
+        </>
+    )
 }
 
 export default Header;
