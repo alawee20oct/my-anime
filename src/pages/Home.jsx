74aaa327b73fee814e388ce1864db72a3dfe1936
alawee20oct <alawee147@hotmail.co.th>
2023-03-12 18:40:29 +0700

    create page and setting page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Content from '../components/Content';
+
+function Home() {
+    return (
+        <>
+            <Content/>
+        </>
+    )
+}
+
+export default Home;
---
+++ src/pages/Setting.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Setting() {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi harum, ducimus eaque suscipit quis ullam aliquam nostrum ab earum tempore expedita numquam beatae accusantium sed repellendus facere at dolore molestiae.
+            Dolorum, magnam. Quibusdam, dolorem libero numquam possimus rem architecto ut omnis reprehenderit? Error placeat, quod ullam modi nostrum voluptatem tempora voluptatibus ut facere cum tenetur ratione rerum quo? Minus, sunt?
+        </div>
+    )
+}
+
+export default Setting;
